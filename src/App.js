6a27ddf39f5f06fdab2d3089a1a0478d7fd0c4f3
Hasan, Noorul <NHasan@cvent.com>
2021-08-22 00:27:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useReducer, useState } from 'react';
+import produce from 'immer';
+
+// library has books => books has author and title => author has name
+const initialState = {
+  books: [
+    {
+      id: 1,
+      title: 'Algebra',
+      author: {
+        name: 'Mark D. Mojave'
+      }
+    }
+  ]
+};
+
+const reducer = produce((draft, action) => {
+  switch(action.type) {
+    case 'add_book': 
+      draft.books.push(action.payload);
+      break;
+    case 'update_book':
+      let book = draft.books.find(b => b.id === action.payload.id);
+      book.title = action.payload.title;
+      book.author.name = action.payload.author.name;
+      break;
+    default: break;
+  }
+});
 
 function App() {
+  const [count, setCount] = useState(2);
+  const [library, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className='content'>
+        <thead><th className='content'>Title</th><th className='content'>Author Name</th></thead>
+        <tbody>
+          {
+            library.books.map(book => {
+              return (
+                <tr key={book.id}>
+                  <td className='content'>
+                    <input 
+                      value={ book.title } 
+                      onChange={e => dispatch({ type: 'update_book', payload: {
+                        ...book,
+                        title: e.target.value
+                      }})}
+                    />
+                  </td>
+                  <td className='content'>
+                    <input value={ book.author.name } onChange={e => dispatch({ type: 'update_book', payload: {
+                        ...book,
+                        author: {
+                          name: e.target.value
+                        }
+                      }})}/>
+                  </td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
+      <br/>
+      <button onClick={() => {
+        setCount(count + 1);
+        dispatch({ type: 'add_book', payload: {
+          id: count,
+          title: '',
+          author: {
+            name: ''
+          }
+        }});
+      }}>Add</button>
     </div>
   );
 }
